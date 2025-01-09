2cbe7a4c9241130471040c3096222f10d1516c65
Jeferson Vale <jefersonnpn@gmail.com>
2025-01-09 17:50:21 -0300

    :truck: improve var names

--- module-4/sparse_table.go
+++ module-4/sparse_table.go
@@ -5,9 +5,9 @@ const (
 	maxK = 25
 )
 
-func RMQ(a []int, queries [][]int) []int {
+func RangeMinimumQuery(a []int, queries [][]int) []int {
 
-	st := [maxN][maxK]int{}
+	sparseTable := [maxN][maxK]int{}
 	n := len(a)
 
 	log := make([]int, maxN+1)
@@ -17,12 +17,12 @@ func RMQ(a []int, queries [][]int) []int {
 	}
 
 	for i := range n {
-		st[i][0] = a[i]
+		sparseTable[i][0] = a[i]
 	}
 
 	for j := 1; j < maxK; j++ {
 		for i := 0; i+(1<<j) <= maxN; i++ {
-			st[i][j] = min(st[i][j-1], st[i+(1<<(j-1))][j-1])
+			sparseTable[i][j] = min(sparseTable[i][j-1], sparseTable[i+(1<<(j-1))][j-1])
 		}
 	}
 
@@ -30,7 +30,7 @@ func RMQ(a []int, queries [][]int) []int {
 	for _, q := range queries {
 		l, r := q[0], q[1]
 		j := log[r-l+1]
-		answers = append(answers, min(st[l][j], st[r-(1<<j)+1][j]))
+		answers = append(answers, min(sparseTable[l][j], sparseTable[r-(1<<j)+1][j]))
 
 	}
 
