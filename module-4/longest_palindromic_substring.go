b4378330ae20c536de9c0ff3b9b1881ad3f60758
Jeferson Vale <jefersonnpn@gmail.com>
2025-01-11 17:27:16 -0300

    :sparkles: add palindromic substring impl.

---
+++ module-4/longest_palindromic_substring.go
@@ -0,0 +1,31 @@
+package module4
+
+func LongestPalindromicSubstring(s string) string {
+
+	n := len(s)
+	memo := make([]bool, n*n)
+
+	for i := range n {
+		memo[i*n+1] = true
+	}
+
+	l, r := 0, 0
+	for i := range n - 1 {
+		if s[i] == s[i+1] {
+			memo[i*n+i+1] = true
+			l, r = i, i+1
+		}
+	}
+
+	for k := 2; k < n; k++ {
+		for i := range n - k {
+			j := i + k
+			if s[i] == s[j] && memo[(i+1)*n+j-1] {
+				memo[i*n+j] = true
+				l, r = i, j
+			}
+		}
+	}
+
+	return s[l : r+1]
+}
