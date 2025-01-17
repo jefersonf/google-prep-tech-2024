a1b42f0c454b97a60c69686b5998a4b013f40803
Jeferson Vale <jefersonnpn@gmail.com>
2025-01-16 22:02:03 -0300

    :sparkles: add leetcode:add-two-numnbers solution

---
+++ module-1/linked-list/add_two_numbers.go
@@ -0,0 +1,27 @@
+package module1
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	currentNode := &ListNode{}
+	headNode := currentNode
+
+	var carryOut int
+
+	for l1 != nil && l2 != nil {
+		sum := l1.Val + l2.Val + carryOut
+		currentNode.Val = sum % 10
+		carryOut = sum / 10
+		if l1.Next != nil || l2.Next != nil {
+			currentNode.Next = &ListNode{}
+			currentNode = currentNode.Next
+		}
+		l1 = l1.Next
+		l2 = l2.Next
+	}
+
+	return headNode
+}
